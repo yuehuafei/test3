bc189b6ae4560cae66ab4b1fa477db469579459a
yuehuafei <1490165179@qq.com>
2017-06-15 13:22:43 +0800

    Create main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your cade here
+  var s=new Array();
+  s=str.split("");
+  var result = 0;
+  for(var i=0;i<s.length;i++){
+    result+=parseInt(s[i]);
+  }
+  return result;
 };
